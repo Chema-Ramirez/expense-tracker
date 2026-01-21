6690323d56986f0c6d3d1e70ba004ede3687eb4f
Jose Ramirez <joseramqui93@gmail.com>
2026-01-21 19:06:30 +0100

    Fix Authorization header typo in expense services

--- client/src/services/expenseServices.js
+++ client/src/services/expenseServices.js
@@ -2,17 +2,34 @@ import axios from "axios";
 
 const API_URL = "http://localhost:4000/api/expenses";
 
+// GET EXPENSES
 export const getExpenses = async (filters = {}, token) => {
-    try {
-        const res = await axios.get(API_URL, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: filters
-        });
+    const res = await axios.get(API_URL, {
+        headers: { Authorization: `Bearer ${token}` },
+        params: filters
+    });
+    return res.data;
+};
 
-        return res.data;
-    } catch (error) {
-        throw new Error("Error fetching expenses")
-    }
+// CREATE EXPENSE
+export const createExpense = async (expenseData, token) => {
+    const res = await axios.post(API_URL, expenseData, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    return res.data;
+};
+
+// DELETE EXPENSE
+export const deleteExpense = async (id, token) => {
+    await axios.delete(`${API_URL}/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+};
+
+// UPDATE EXPENSE
+export const updateExpense = async (id, updatedData, token) => {
+    const res = await axios.put(`${API_URL}/${id}`, updatedData, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    return res.data;
 };
