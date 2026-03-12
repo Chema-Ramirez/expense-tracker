23e3a0613bf4c1bd3d662950f55be785c8eaccfe
Jose Ramirez <joseramqui93@gmail.com>
2026-03-12 12:03:26 +0100

    explicit cors origins

--- server/src/app.js
+++ server/src/app.js
@@ -36,7 +36,12 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: ["https://bitoink.netlify.app", "http://localhost:5173"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // MIDDLEWARE JSON
 app.use(express.json());
